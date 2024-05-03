9a7042e78608319a27dd4f0195cf3272a02b9158
Armaan <armaansinghbhau8@gmail.com>
2024-05-03 09:44:08 +0530

    function demo

---
+++ Functions/function_demo.java
@@ -0,0 +1,12 @@
+package Functions;
+public class function_demo {
+
+    public static void SayHi(String name){
+        System.out.println("Hello, " + name);
+    }
+    public static void main(String[] args) {
+        SayHi("John Doe");
+    }
+}
+    
+
