93007511de793bbae4990da2c8ed440e5e1e1090
Armaan <armaansinghbhau8@gmail.com>
2024-05-03 09:49:21 +0530

    pair_add_elements

---
+++ arrays/pair_add_elements.java
@@ -0,0 +1,39 @@
+package arrays;
+
+import java.util.*;
+
+public class pair_add_elements {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        System.out.print("Enter the length: ");
+        int n = sc.nextInt();
+
+        for(int i=0; i<n; i++){
+            System.out.print("Enter the " + i +" element: ");
+            Integer num = sc.nextInt();
+            arr.add(num);
+        }
+
+        System.out.print("Enter the sum of which you find the combination of: ");
+        int targetsum = sc.nextInt();
+
+        boolean found = false;
+        
+        for(int i=0; i<n; i++){
+            for(int j = i + 1; j<n; j++){
+                if(arr.get(i) + arr.get(j) == targetsum){
+                    System.out.println("Pair found at (" + arr.get(i) + "," + arr.get(j) + ")");
+                    found = true;
+                }
+            }
+        }
+
+        if(!found){
+            System.out.println("No pair found");
+        }
+
+        sc.close();
+    }
+}
