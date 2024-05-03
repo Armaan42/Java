4eae8278a49d3d07ab1f2e9764a26004bf82b374
Armaan <armaansinghbhau8@gmail.com>
2024-05-03 09:47:30 +0530

    sorting

---
+++ arrays/sorting.java
@@ -0,0 +1,13 @@
+package arrays;
+
+import java.util.*;
+
+public class sorting {
+    public static void main(String[] args) {
+
+        int arr[] = {2,6,-2,5,34,61,0,4,3};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+        
+    }
+}
